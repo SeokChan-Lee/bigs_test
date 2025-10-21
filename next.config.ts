76aed908546456cac3e16f3f20c281f539a4058c
SeokChan-Lee <melee0826@gmail.com>
2025-10-22 03:51:52 +0900

    📦 Chore : 패키지 추가 및 이미지(로고,파비콘) 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
+
+function getHostname(url: string) {
+  try {
+    const u = new URL(url);
+    return u.hostname;
+  } catch {
+    return "";
+  }
+}
+
+const hostname = getHostname(NEXT_PUBLIC_BASE_URL);
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: hostname ? [hostname] : [],
+  },
 };
 
 export default nextConfig;
